593b18d523fd70c63779aaa0760e428294fc63d2
Fabio Lopes <fabioglopes@gmail.com>
2025-03-03 21:21:55 -0300

    initial

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,79 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[ show edit update destroy ]
+
+  # GET /items or /items.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /items/1 or /items/1.json
+  def show
+  end
+
+  # GET /items/new
+  def new
+    @item = Item.new
+  end
+
+  # GET /items/1/edit
+  def edit
+  end
+
+  # POST /items or /items.json
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: "Item was successfully created." }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /items/1 or /items/1.json
+  def update
+    if @item.toggle!
+      respond_to do |format|
+        #format.turbo_stream
+        format.html { redirect_to items_path }
+      end
+    end
+  end
+
+  # DELETE /items/1 or /items/1.json
+  def destroy
+    @item.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to items_path, status: :see_other, notice: "Item was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  def toggle_status
+    @item = Item.find(params[:id])
+    new_status = @item.toggle!
+    @item.update(status: new_status)
+
+    respond_to do |format|
+      #format.turbo_stream { render turbo_stream: turbo_stream.replace(@item) }
+      format.html { redirect_to items_path }
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def item_params
+      params.expect(item: [ :sku, :status ])
+    end
+end
---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ApplicationRecord
+  broadcasts_refreshes
+  #after_update_commit -> { broadcast_refresh_to("items")}
+
+  include AASM
+
+  aasm column: :status do
+    state :pending, initial: true
+    state :approved
+
+    event :approve do
+      transitions from: :pending, to: :approved
+    end
+
+    event :pending do
+      transitions from: :approved, to: :pending
+    end
+
+    event :toggle do
+      transitions from: :pending, to: :approved
+      transitions from: :approved, to: :pending
+    end
+  end
+end
