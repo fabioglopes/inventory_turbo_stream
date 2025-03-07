e138063da39e610b14baaf29074e63c0db082f33
Fabio Lopes <fabioglopes@gmail.com>
2025-03-07 14:53:18 -0300

    Enable Turbo Streams for items with `turbo_stream_from`.
    Replaced manual broadcasting logic with `broadcasts_refreshes` in the `Item` model for a more streamlined approach. Added `turbo_stream_from` to the `_item.html.erb` partial to enable real-time updates through Turbo Streams.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
-  #broadcasts_refreshes
-  after_commit -> { broadcast_refresh_to("items") }
+  broadcasts_refreshes
+  #
+  # broadcast_refreshes will call all those callbacks update and destroy will be to the self
+  #after_create_commit  -> { broadcast_refresh_later_to "items" }
+  #after_update_commit  -> { broadcast_refresh_later }
+  #after_destroy_commit -> { broadcast_refresh }
+  #
+
+  #after_update_commit -> {
+  #  if saved_change_to_status?
+  #    broadcast_refresh_later_to "items"
+  #  else
+  #    broadcast_refresh_later
+  #  end
+  #}
+
+  #after_commit -> { broadcast_refresh_later_to("items") }
 
   validates_presence_of :sku
 
